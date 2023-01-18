d719fb222a4123ec124c3694ef45bba7c2e281e6
vinay <vinaykumarbeelagi@gmail.com>
2023-01-18 09:53:41 +0530

    todo_app updated upto Todoconatainer,todolist,todoitem

--- src/Components/InputTodo.js
+++ src/Components/InputTodo.js
@@ -1,9 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from "react"
 
-export default class InputTodo extends Component {
+class InputTodo extends Component {
+  state = {
+    title: ""
+  };
+  onChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
   render() {
     return (
-      <div>InputTodo</div>
+      <form>
+        <input type="text" placeholder="Add todo..." value={this.state.title}
+        name="title"
+        onChange={this.onChange} />
+        <button>Submit</button>
+      </form>
     )
   }
 }
+export default InputTodo
